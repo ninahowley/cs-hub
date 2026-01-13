c14e7b1431c7b1ed12536a2bcfe6a069f046beba
propadiene-1 <aileenliang06@gmail.com>
2026-01-13 14:46:26 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, flash, url_for, render_template
+
+app = Flask(__name__)
+app.secret_key = "temp"
+
+@app.route("/")
+def index():
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        print(e)
+        flash('An error occurred. Please try agåain.')
+        return
+
+@app.route("/major-plan")
+def major_plan():
+    try:
+        return render_template('major-plan.html')
+    except Exception as e:
+        print(e)
+        flash('An error occurred. Please try again.')
+        return
+
+@app.route("/cs-map")
+def cs_map():
+    try:
+        return render_template('cs-map.html')
+    except Exception as e:
+        print(e)
+        flash('An error occurred. Please try again.')
+        return
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
